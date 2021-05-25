babaecbd4586d92d6b89de4cf49c7f4796be6009
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2021-05-25 13:53:14 -0400

    fundamentals begin

---
+++ part-1/assignment.js
@@ -0,0 +1,15 @@
+let country = 'Greece';
+let continent = 'Europe';
+let population = '10.72m';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
---
+++ part-1/script.js
@@ -0,0 +1,55 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 20);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Mariaaaaa";
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//let jonas_matilda = 'JM' // we can do this
+//let jonas&matilda = 'JM' //But not this. no symbols other than '$' and '_' use us Var on JS
+
+let myFirstJob = "Carpenter"
+let mySecondJob = "Server"
+let myCurrentJob = "Unemployment"
+
+console.log(myFirstJob)
+console.log(mySecondJob)
+console.log(myCurrentJob)
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1986;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1987;
+// birthYear = 1990; //that gives an error because we can't change the Value of a const variable.
+
+const job; //that gives an error also, we can't have an empty value const variable.
