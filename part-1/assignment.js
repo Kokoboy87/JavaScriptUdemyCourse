ec8860c6dedb97046d45c589157943934cdcc810
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2021-05-25 17:55:21 -0400

    operators

--- part-1/assignment.js
+++ part-1/assignment.js
@@ -1,12 +1,19 @@
 const country = 'Greece';
 const continent = 'Europe';
-let population = '10.72m';
+let population = 10.72;
 const isIsland = false;
 let language;
 language = 'Ελληνίκα';
+population++;
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 
 // console.log(typeof isIsland);
 // console.log(typeof country);
 // console.log(typeof population);
 // console.log(typeof language);
-console.log(language);
+// console.log(language);
+// console.log(population / 2);
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// console.log(description);
--- part-1/script.js
+++ part-1/script.js
@@ -23,6 +23,7 @@ console.log(myFirstJob)
 console.log(mySecondJob)
 console.log(myCurrentJob)
 
+//======== Data types ========//
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -45,7 +46,7 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-
+//======= let, const and var =======//
 let age = 30;
 age = 31;
 
@@ -53,4 +54,55 @@ const birthYear = 1987;
 // birthYear = 1990; //that gives an error because we can't change the Value of a const variable.
 
 const job; //that gives an error also, we can't have an empty value const variable.
+
+
+//-------------------------------//
+//-----  BASIC OPERATORS  ------//
+//-----------------------------//
+
+//====== Math Operators ======//
+const now = 2021;
+const ageGeorgios = now - 1987;
+const ageMaria = now - 2003;
+console.log(ageGeorgios, ageMaria);
+
+console.log(ageGeorgios * 2, ageGeorgios / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+firstName = 'Georgios';
+lastName = 'kokotinis';
+console.log(firstName + ' ' + lastName);
+
+//====== Assignment operators =======//
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 255
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+//===== Comparison operators =====//
+console.log(ageGeorgios > ageMaria); // >, <, >=, <=
+console.log(ageMaria >= 18);
+
+const isFullAge = ageMaria >= 18;
+
+console.log(now - 1987 > now - 2003);
+
+
+//====== Math Operators ======//
+
+const now = 2021;
+const ageGeorgios = now - 1987;
+const ageMaria = now - 2003;
+
+console.log(now - 1987 > now - 2003);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageGeorgios + ageMaria) / 2;
+console.log(ageGeorgios, ageMaria, averageAge);
 */
